784027d2c99aa57342220429ebe8dd2c6e4a00a4
denandrapr <denandra1628@gmail.com>
2025-08-08 22:37:51 +0700

    chore[deps] setup hilt project

--- app/src/main/java/com/example/denandra_hanabank_test/ui/MainActivity.kt
+++ app/src/main/java/com/example/denandra_hanabank_test/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.denandra_hanabank_test
+package com.example.denandra_hanabank_test.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.denandra_hanabank_test.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
